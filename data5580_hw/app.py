42007e3cfe7c7a474b4a91d64dd8d55febbaaf3f
richard-woolston <richard.woolston@usu.edu>
2026-01-11 15:16:49 -0700

    Week 1 checkpoint

--- data5580_hw/app.py
+++ data5580_hw/app.py
@@ -1,7 +1,33 @@
+
+# Standardlibrary
+import uuid
+
+# Installed
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+# Local
+
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db = SQLAlchemy(app)
+
+    class User(db.Model):
+        __tablename__ = "users"
+        id = db.Column(db.String, primary_key=True)
+        name = db.Column(db.String)
+        email = db.Column(db.String)
+
+
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
 
     @app.route("/", methods=["GET"])
     def home():
@@ -18,7 +44,29 @@ def create_app():
 
         return jsonify({"result": a + b})
 
+    @app.route("/user/{string: user_id}", methods=["GET"])
+    def get_user_by_id(user_id: str):
+
+        User.query.filter_by(id=user_id).first()
+
+    @app.route("/user", methods=["POST"])
+    def create_user():
+        data = request.get_json()
+
+        name = data.get("name")
+        email = data.get("email")
+        id = uuid.uuid4().hex
+
+        user = User(name=name, email=email, id=id)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({"name": name, "email": email, "id": id}), 200
+
     return app
+
+
 
 
 if __name__ == "__main__":
