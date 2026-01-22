71514b1753f1f71bd06421e4d8e491a52f982429
richard-woolston <richard.woolston@usu.edu>
2026-01-21 20:13:18 -0700

    Homework 3 checkpoint with fixed tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,9 @@
+from unittest import TestCase
+
 import pytest
 
 from data5580_hw.app import create_app
+
 
 @pytest.fixture
 def client():
@@ -22,4 +25,33 @@ def test_create_user(client):
     assert response.status_code == 200
 
 
+def test_get_user(client):
+    payload = {"email": "test@example.com", "name": "test"}
+    response = client.post("/user", json=payload)
+    assert response.status_code == 200
 
+    id = response.get_json(force=True)['id']
+    assert id is not None
+
+    payload['id'] = id
+    response = client.get("/user/{}".format(id))
+    TestCase().assertDictEqual(response.get_json(force=True), payload)
+
+
+def test_update_user(client):
+    payload = {"email": "test@example.com", "name": "test"}
+    response = client.post("/user", json=payload)
+    assert response.status_code == 200
+
+    id = response.get_json(force=True)['id']
+    assert id is not None
+
+    payload = {"email": "test@example.com", "name": "test2"}
+    response = client.patch("/user/{}".format(id), json=payload)
+    assert response.status_code == 200
+
+    payload['id'] = id
+    response = client.get("/user/{}".format(id))
+    TestCase().assertDictEqual(response.get_json(force=True), payload)
+
+    assert response.get_json(force=True)["name"] == "test2"
