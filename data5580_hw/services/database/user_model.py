0c596041fdb279aa78fffe8b9c441a4f60915348
TheRenster <laurencknox@gmail.com>
2026-01-22 16:45:13 -0700

    "Week 3 checkpoint commit"

--- data5580_hw/services/database/user_model.py
+++ data5580_hw/services/database/user_model.py
@@ -8,21 +8,21 @@ from data5580_hw.models.user_model import User as UserSQL
 from data5580_hw.services.user import User
 
 
-class UserController:    d
-def __init__(self, user_service):
-        self.user_service = user_service    
-def create_user(self, data):
-        name = data.get("username")
-        email = data.get("email")
+class UserController:    
+    def __init__(self, user_service):
+            self.user_service = user_service    
+    def create_user(self, data):
+            name = data.get("username")
+            email = data.get("email")
 
-        if not name or not email:
-            return {"error": "Missing values"}, 400
+            if not name or not email:
+                return {"error": "Missing values"}, 400
 
-        user = self.user_service.create_user(name, email)
-        if not user:
-            return {"error": "Email already exists"}, 400
+            user = self.user_service.create_user(name, email)
+            if not user:
+                return {"error": "Email already exists"}, 400
 
-        return {
-            "username": user.name,
-            "email": user.email
-        }, 200
+            return {
+                "username": user.name,
+                "email": user.email
+            }, 200
