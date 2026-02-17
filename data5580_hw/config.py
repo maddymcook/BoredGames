c1b3454a3ec85fab0e5800474702053d52ea6a4f
TheRenster <laurencknox@gmail.com>
2026-02-17 13:55:15 -0700

    Add configuration class for MLFlow settings

---
+++ data5580_hw/config.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+
+class Config:
+
+    # MLFlow
+    TRACKING_URI = "http://localhost:8080"
+    MODELS = {
+        "california-housing": {
+            "2": {
+                "model": None
+                , "model_type": "REGRESSION"
+                , "mlflow_flavor": "sklearn"
+            }
+        }
+    }
+
+environments = {
+    'config': Config()
+}
