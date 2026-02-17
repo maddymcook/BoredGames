27650fa2cb86202c6ac5fafab70b5a80ae7cce36
TheRenster <laurencknox@gmail.com>
2026-02-17 14:05:49 -0700

    Add Prediction and Model classes for predictions

---
+++ data5580_hw/models/prediction.py
@@ -0,0 +1,36 @@
+from typing import Dict, Union, Optional
+
+from datetime import datetime
+import pandas as pd
+
+from pydantic import Field
+
+from data5580_hw.models.base import LocalBaseModel, get_id
+
+
+class Model(LocalBaseModel):
+    name: str
+    version: str
+    type: str
+
+    _model = None
+    id: str = Field(default_factory=get_id)
+
+
+class Prediction(LocalBaseModel):
+
+    id: str = Field(default_factory=get_id)
+    features: Dict = Field(default_factory=dict)
+    tags: Dict = Field(default_factory=dict)
+    label: Union[int, float, str, None] = None
+    actual: Union[int, float, str, None] = None
+    threshold: Optional[float] = None
+
+    model: Optional[Model] = None
+
+    created: datetime = Field(default_factory=datetime.now)
+    updated: datetime = Field(default_factory=datetime.now)
+
+    def get_pandas_frame_of_inputs(self):
+
+        return pd.DataFrame([self.features], index=[0])
